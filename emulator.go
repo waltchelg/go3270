f0abd92f3534b294f2d6cef02f61ded6ef8711a5
Chris Walter <christopher_walter@na.honda.com>
2025-07-07 12:38:17 -0400

    remove validkeys, and swap to wc3270

--- emulator.go
+++ emulator.go
@@ -28,27 +28,27 @@ const (
 	F12   = "PF(12)"
 )
 
-//Emulator base struct to x3270 terminal emulator
+// Emulator base struct to x3270 terminal emulator
 type Emulator struct {
 	Host       string
 	Port       int
 	ScriptPort string
 }
 
-//moveCursor move cursor to especific row(x) and column(y)
+// moveCursor move cursor to especific row(x) and column(y)
 func (e *Emulator) moveCursor(x, y int) error {
 	command := fmt.Sprintf("MoveCursor(%d,%d)", x, y)
 	return e.execCommand(command)
 }
 
-//SetString fill field with value passed by parameter
-//setString will fill the field that the cursor is marked
+// SetString fill field with value passed by parameter
+// setString will fill the field that the cursor is marked
 func (e *Emulator) SetString(value string) error {
 	command := fmt.Sprintf("String(%s)", value)
 	return e.execCommand(command)
 }
 
-//GetRows returns the number of rows in the saved screen image.
+// GetRows returns the number of rows in the saved screen image.
 func (e *Emulator) GetRows() (int, error) {
 	s, err := e.execCommandOutput("Snap(Rows)")
 	if err != nil {
@@ -61,7 +61,7 @@ func (e *Emulator) GetRows() (int, error) {
 	return i, nil
 }
 
-//GetColumns returns the number of columns in the saved screen image.
+// GetColumns returns the number of columns in the saved screen image.
 func (e *Emulator) GetColumns() (int, error) {
 	s, err := e.execCommandOutput("Snap(Cols)")
 	if err != nil {
@@ -74,8 +74,8 @@ func (e *Emulator) GetColumns() (int, error) {
 	return i, nil
 }
 
-//FillString fill the field by your position
-//row(x), column(y) value(value to be filled)
+// FillString fill the field by your position
+// row(x), column(y) value(value to be filled)
 func (e *Emulator) FillString(x, y int, value string) error {
 	if err := e.moveCursor(x, y); err != nil {
 		return fmt.Errorf("error to move cursor: %v", err)
@@ -83,7 +83,7 @@ func (e *Emulator) FillString(x, y int, value string) error {
 	return e.SetString(value)
 }
 
-//Press press a keyboard key
+// Press press a keyboard key
 func (e *Emulator) Press(key string) error {
 	if !e.validaKeyboard(key) {
 		return fmt.Errorf("invalid key %s", key)
@@ -91,19 +91,20 @@ func (e *Emulator) Press(key string) error {
 	return e.execCommand(key)
 }
 
-//validaKeyboard valid if key passed by parameter if a key valid
+// validaKeyboard valid if key passed by parameter if a key valid
 func (e *Emulator) validaKeyboard(key string) bool {
-	switch key {
+	/*switch key {
 	case Tab:
 		return true
 	case Enter:
 		return true
 	default:
 		return false
-	}
+	}*/
+	return true
 }
 
-//IsConnected check if a connection with host exist
+// IsConnected check if a connection with host exist
 func (e *Emulator) IsConnected() bool {
 	s, err := e.query("ConnectionState")
 	if err != nil || len(strings.TrimSpace(s)) == 0 {
@@ -112,19 +113,19 @@ func (e *Emulator) IsConnected() bool {
 	return true
 }
 
-//GetValue return a value by position
-//row(x), column(y), lenght(l)
+// GetValue return a value by position
+// row(x), column(y), lenght(l)
 func (e *Emulator) GetValue(x, y, l int) (string, error) {
 	command := fmt.Sprintf("Ascii(%d,%d,%d)", x, y, l)
 	return e.execCommandOutput(command)
 }
 
-//CursorPosition return actual position by cursor
+// CursorPosition return actual position by cursor
 func (e *Emulator) CursorPosition() (string, error) {
 	return e.query("cursor")
 }
 
-//Connect open an connection with x3270 and host
+// Connect open an connection with x3270 and host
 func (e *Emulator) Connect() error {
 	if e.Host == "" {
 		return errors.New("Host need to be filled")
@@ -143,7 +144,7 @@ func (e *Emulator) Connect() error {
 	return nil
 }
 
-//Disconnect close connection with x3270
+// Disconnect close connection with x3270
 func (e *Emulator) Disconnect() error {
 	if e.IsConnected() {
 		return e.execCommand("quit")
@@ -151,15 +152,15 @@ func (e *Emulator) Disconnect() error {
 	return nil
 }
 
-//query returns state information from x3270
+// query returns state information from x3270
 func (e *Emulator) query(keyword string) (string, error) {
 	command := fmt.Sprintf("query(%s)", keyword)
 	return e.execCommandOutput(command)
 }
 
-//createApp create a connection of 3270 with host
+// createApp create a connection of 3270 with host
 func (e *Emulator) createApp() {
-	cmd := exec.Command("x3270", "-scriptport", e.ScriptPort, e.hostname())
+	cmd := exec.Command("wc3270", "-scriptport", e.ScriptPort, e.hostname())
 	go func() {
 		if err := cmd.Run(); err != nil {
 			log.Fatalf("error to create an instance of 3270\n%v\n", err)
@@ -168,12 +169,12 @@ func (e *Emulator) createApp() {
 	time.Sleep(6 * time.Second)
 }
 
-//hostname return hostname formatted
+// hostname return hostname formatted
 func (e *Emulator) hostname() string {
 	return fmt.Sprintf("%s:%d", e.Host, e.Port)
 }
 
-//execCommand executes a command on the connected x3270 instance
+// execCommand executes a command on the connected x3270 instance
 func (e *Emulator) execCommand(command string) error {
 	cmd := exec.Command("x3270if", "-t", e.ScriptPort, command)
 	if err := cmd.Run(); err != nil {
@@ -183,7 +184,7 @@ func (e *Emulator) execCommand(command string) error {
 	return nil
 }
 
-//execCommand executes a command on the connected x3270 instance and return output
+// execCommand executes a command on the connected x3270 instance and return output
 func (e *Emulator) execCommandOutput(command string) (string, error) {
 	cmd := exec.Command("x3270if", "-t", e.ScriptPort, command)
 	b, err := cmd.Output()
